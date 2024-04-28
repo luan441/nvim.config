6503a5c710cd6cdee47eafaf00f8fc4289ae1499
luan441 <luanhenrique441@gmail.com>
2024-04-28 08:39:04 -0300

    first commit

---
+++ lua/mappings.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<C-s>', ':w<CR>', {})
+vim.keymap.set('n', '<C-q>', ':q<CR>', {})
---
+++ lua/plugins/debug.lua
@@ -0,0 +1,36 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		{
+			"rcarriga/nvim-dap-ui",
+			dependencies = {
+				"nvim-neotest/nvim-nio",
+			},
+		},
+    "leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+
+    dapui.setup()
+    require('dap-go').setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+    vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+	end,
+}
